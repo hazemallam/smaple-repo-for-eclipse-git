255280e14163ffc8878f14048e3db680cb088203
Hazem Allam <s.enghazemallm@gmail.com>
2021-07-07 19:32:33 +0200

    adding setters and getters to User

--- src/com/hazem/model/User.java
+++ src/com/hazem/model/User.java
@@ -3,4 +3,13 @@ package com.hazem.model;
 public class User {
 
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
